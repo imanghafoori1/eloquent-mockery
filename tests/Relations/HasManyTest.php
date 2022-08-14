22fd2fc264e04bfff06bbc5d9ee1408f638e874c
Iman Ghafoori <imanghafoori1@gmail.com>
2022-08-14 22:27:49 +0430

    Add test for eagerLoading

--- tests/Relations/HasManyTest.php
+++ tests/Relations/HasManyTest.php
@@ -43,10 +43,10 @@ class HasManyTest extends TestCase
         HasManyUser::addFakeRow(['id' => 3, 'name' => 'Iman 3']);
         HasManyUser::addFakeRow(['id' => 4, 'name' => 'Iman 4']);
 
-        HasManyComment::addFakeRow(['id' => 1, 'user_id' => 1,'comment' => 'sss']);
-        HasManyComment::addFakeRow(['id' => 2, 'user_id' => 1,'comment' => 'aaa']);
-        HasManyComment::addFakeRow(['id' => 3, 'user_id' => 2,'comment' => 'bbb']);
-        HasManyComment::addFakeRow(['id' => 4, 'user_id' => 2,'comment' => 'ccc']);
+        HasManyComment::addFakeRow(['id' => 1, 'user_id' => 1, 'comment' => 'sss']);
+        HasManyComment::addFakeRow(['id' => 2, 'user_id' => 1, 'comment' => 'aaa']);
+        HasManyComment::addFakeRow(['id' => 3, 'user_id' => 2, 'comment' => 'bbb']);
+        HasManyComment::addFakeRow(['id' => 4, 'user_id' => 2, 'comment' => 'ccc']);
         HasManyComment::addFakeRow(['id' => 5, 'user_id' => 3, 'comment' => 'ddd']);
 
         $user = HasManyUser::with('comments')->where('id', 1)->first();
@@ -56,6 +56,19 @@ class HasManyTest extends TestCase
         $this->assertInstanceOf(HasManyComment::class, $user->comments[0]);
         $this->assertInstanceOf(HasManyComment::class, $user->comments[1]);
         $this->assertEquals(2, $user->comments[1]->id);
+        $this->assertEquals(1, $user->comments[0]->id);
+        $this->assertEquals(1, $user->comments[0]->user_id);
+        $this->assertEquals('sss', $user->comments[0]->comment);
+
+        $user = HasManyUser::with('comments:user_id')->where('id', 1)->first();
+
+        $this->assertInstanceOf(Collection::class, $user->comments);
+        $this->assertEquals(2, $user->comments->count());
+        $this->assertInstanceOf(HasManyComment::class, $user->comments[0]);
+        $this->assertInstanceOf(HasManyComment::class, $user->comments[1]);
+        $this->assertEquals(null, $user->comments[1]->id);
+        $this->assertEquals(1, $user->comments[1]->user_id);
+        $this->assertEquals(null, $user->comments[1]->comment);
 
         $user = HasManyUser::with('comments')->where('id', 1)->get()->first();
 
@@ -102,7 +115,7 @@ class HasManyTest extends TestCase
         $this->assertEquals(1, HasManyComment::query()->find(3)->user()->count());
 
         $newUser = HasManyComment::query()->find(3)->user()->create([
-            'name' => 'created'
+            'name' => 'created',
         ]);
 
         $this->assertNotNull($newUser->created_at);
@@ -115,9 +128,8 @@ class HasManyTest extends TestCase
         $this->assertSame(HasManyUser::getCreatedModel(), $newUser);
         $this->assertEquals(5, HasManyUser::count());
 
-
         $comment = HasManyUser::find(4)->comments()->create([
-            'comment' => 'created!'
+            'comment' => 'created!',
         ]);
         $this->assertEquals('created!', $comment->comment);
         $this->assertEquals(6, $comment->id);
