215d75cc0a808850a9d03b690a40377c2d200a14
Iman Ghafoori <imanghafoori1@gmail.com>
2022-08-15 11:23:36 +0430

    add tests for whereNotIn

--- tests/WhereInTest.php
+++ tests/WhereInTest.php
@@ -22,7 +22,7 @@ class WhereInTest extends TestCase
     /**
      * @test
      */
-    public function whereIn()
+    public function where_in()
     {
         WhereInUser::addFakeRow(['id' => 1, 'name' => 'Iman 1', 'age' => 20,]);
         WhereInUser::addFakeRow(['id' => 2, 'name' => 'Iman 2', 'age' => 30,]);
@@ -52,7 +52,40 @@ class WhereInTest extends TestCase
         $this->assertEquals(true, $user->exists);
         $this->assertInstanceOf(WhereInUser::class, $user);
         $this->assertEquals(2, $users->count());
+    }
 
-        WhereInUser::stopFaking();
+    /**
+     * @test
+     */
+    public function where_not_in()
+    {
+        WhereInUser::addFakeRow(['id' => 1, 'name' => 'Iman 1', 'age' => 20,]);
+        WhereInUser::addFakeRow(['id' => 2, 'name' => 'Iman 2', 'age' => 30,]);
+        WhereInUser::addFakeRow(['id' => 3, 'name' => 'Iman 3', 'age' => 34,]);
+
+        // ################ where not In / first ################
+        $user = WhereInUser::whereNotIn('id', [1, 3])->first();
+        $this->assertEquals(2, $user->id);
+        $this->assertEquals('Iman 2', $user->name);
+        $this->assertInstanceOf(WhereInUser::class, $user);
+
+        // ################  where not In / get  ################
+        $users = WhereInUser::whereNotIn('id', [3])->get();
+        $this->assertInstanceOf(Collection::class, $users);
+
+        $user = $users[0];
+        $this->assertEquals(1, $user->id);
+        $this->assertEquals('Iman 1', $user->name);
+        $this->assertEquals(20, $user->age);
+        $this->assertEquals(true, $user->exists);
+        $this->assertInstanceOf(WhereInUser::class, $user);
+
+        $user = $users[1];
+        $this->assertEquals(2, $user->id);
+        $this->assertEquals('Iman 2', $user->name);
+        $this->assertEquals(30, $user->age);
+        $this->assertEquals(true, $user->exists);
+        $this->assertInstanceOf(WhereInUser::class, $user);
+        $this->assertEquals(2, $users->count());
     }
 }
