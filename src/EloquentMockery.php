a343e5b4919abe406c530bfdd4b2c16ff8cc1299
miladev-ent <milwad.dev@gmail.com>
2023-01-07 15:50:15 +0330

    add fakeModels function

--- src/EloquentMockery.php
+++ src/EloquentMockery.php
@@ -2,7 +2,18 @@
 
 namespace Imanghafoori\EloquentMockery;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
 class EloquentMockery
 {
-
+    public static function fakeModels(array $models)
+    {
+        foreach ($models as $model) {
+            if (is_string($model)) {
+                (new $model)->fake();
+            } else {
+                throw new ModelNotFoundException;
+            }
+        }
+    }
 }
