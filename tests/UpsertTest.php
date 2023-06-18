7e44327807f0eba13084b5bea8843700c8af7756
Iman Ghafoori <imanghafoori1@gmail.com>
2023-06-18 12:48:15 +0330

    Add tests for upsert

--- tests/UpsertTest.php
+++ tests/UpsertTest.php
@@ -4,6 +4,7 @@ namespace Imanghafoori\EloquentMockery\Tests;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 use Imanghafoori\EloquentMockery\FakeDB;
 use PHPUnit\Framework\TestCase;
 
@@ -26,24 +27,63 @@ class UpsertTest extends TestCase
         UpsertModel::query()->upsert([
             'name' => 'a1',
             'age' => 12,
+            'weight' => 50,
         ], ['name']);
+
+        $this->assertEquals(1, UpsertModel::query()->count());
+
         UpsertModel::query()->upsert([
             'name' => 'a',
             'age' => 10,
+            'weight' => 60,
         ], ['name']);
+
+        // Assert is inserted:
+        $this->assertEquals(2, UpsertModel::query()->count());
+
         UpsertModel::query()->upsert([
             'name' => 'a',
             'age' => 11,
+            'weight' => 66,
         ], ['name']);
+
+        // Assert is updated:
+        $this->assertEquals(2, UpsertModel::query()->count());
+
+        $upsert = UpsertModel::query()->get();
+        $this->assertEquals(66, $upsert[1]->weight);
+        $this->assertEquals(11, $upsert[1]->age);
+        $this->assertEquals(Carbon::now()->format('Y-m-d H:i'), $upsert[1]->created_at->format('Y-m-d H:i'));
+        $this->assertEquals(Carbon::now()->format('Y-m-d H:i'), $upsert[1]->updated_at->format('Y-m-d H:i'));
+
         UpsertModel::query()->upsert([
             'name' => 'a',
             'age' => 11,
+            'weight' => 70,
         ], ['name', 'age']);
 
+        // Assert is updated:
         $this->assertEquals(2, UpsertModel::query()->count());
         $upsert = UpsertModel::query()->get();
         $this->assertEquals(12, $upsert[0]->age);
         $this->assertEquals(11, $upsert[1]->age);
+        $this->assertEquals(70, $upsert[1]->weight);
+
+        UpsertModel::query()->upsert([
+            'name' => 'a3',
+            'age' => 11,
+            'weight' => 70,
+        ], ['name', 'age']);
+
+        $this->assertEquals(3, UpsertModel::query()->count());
+
+        UpsertModel::query()->upsert([
+            'name' => 'a3',
+            'age' => 14,
+            'weight' => 70,
+        ], ['name', 'age']);
+
+        $this->assertEquals(4, UpsertModel::query()->count());
 
         FakeDB::dontMockQueryBuilder();
     }
